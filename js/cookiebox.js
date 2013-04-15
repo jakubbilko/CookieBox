880bcd5888dd834b1c1738184aa1eadfe9243366
kozulowski <jakub@kozulowski.pl>
2013-04-15 12:29:28 +0200

    Initial JS commit.

---
+++ js/cookiebox.js
@@ -0,0 +1,43 @@
+/*!
+* jQuery CookieBox Plugin v0.1.0
+* https://github.com/kozulowski/CookieBox
+*
+* Copyright 2013 Jakub Bilko
+* Released under the MIT license
+*/
+
+(function ($) {
+	$.fn.cookieBox = function(options) {
+		
+		var box = this;
+		
+		var settings = $.extend({
+			'delay': 1000,
+			'visible': false,
+			'showspeed': 1000,
+			'hidespeed': 1000,
+			'autocss': false,
+			'cookiename': 'cookiebox'
+		}, options);
+		
+		if(settings.autocss) {
+				box.css('position', 'fixed');
+				box.css('top', 0);
+				box.css('left', 0)
+				box.css('display', 'none');
+		}		
+		
+		if(!$.cookie(settings.cookiename)) {
+			this.find('.cookiebox-close').click(function(event) {
+				box.slideUp(settings.hidespeed);
+				$.cookie(settings.cookiename, 'viewed');
+			});
+		
+			if(!settings.visible) { 
+				box.delay(settings.delay).slideDown(settings.showspeed);
+			}
+		}
+		
+		
+	}
+})(jQuery);
